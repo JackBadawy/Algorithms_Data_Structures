20f14dea36768a6be66959eead4247b8b5d9a5da
Jack <jackbadawy1999@gmail.com>
2024-11-10 22:00:36 +1100

    continuous compounding implementation + enum for frequency

--- Finance_Algorithms/Compound_Interest/compound_interest.c
+++ Finance_Algorithms/Compound_Interest/compound_interest.c
@@ -5,15 +5,40 @@ float power_of(float num, int power)
 {
     float pow = 1;
     int i;
-    for (i = 0; i < power; i++) {
-        pow = pow * num;
-    }
+    for (i = 0; i < power; i++) pow = pow * num;
+
     return pow;
 }
 
-float calculate_compound_interest(float principal, float rate, int time)
+float calculate_e_power(float x)
 {
-    float real_rate = rate * 0.01;
-    real_rate = real_rate / 12;
-    return principal * power_of(1 + real_rate, time);
+    float sum = 1.0;
+    float term = 1.0;
+    int i;
+
+    for (i = 1; i <= 10; i++)
+    {
+        term *= x / i;
+        sum += term;
+    }
+    return sum;
+}
+
+float calculate_compound_interest(float principal, float rate, int time_months, CompoundingFrequency freq)
+{
+    float annual_rate = rate * 0.01;
+    float time_years = time_months / 12.0;
+
+    if (freq == CONTINUOUS)
+    {
+       return principal * calculate_e_power(annual_rate * time_years);
+    }
+    else
+    {
+
+        float n = (float)freq;
+        float periodic_rate = annual_rate / n;
+        int periods = n * time_years;
+        return principal * power_of(1 + periodic_rate, periods);
+    }
 }
--- Finance_Algorithms/Compound_Interest/demo.c
+++ Finance_Algorithms/Compound_Interest/demo.c
@@ -3,17 +3,35 @@
 
 int main() {
     float principal, rate, result;
-    int time;
+    int time, freq_choice;
+    CompoundingFrequency freq;
 
-    printf("What is your principle amount?\n$");
+    printf("What is your principal amount?\n");
     scanf("%f", &principal);
-    printf("What is the interest rate PA?\n");
+
+    printf("\nWhat is the interest rate PA?\n");
     scanf("%f", &rate);
-    printf("How many months has interest been compounding?\n");
+
+    printf("\nHow many months has interest been compounding?\n");
     scanf("%d", &time);
 
-    result = calculate_compound_interest(principal, rate, time);
-    printf("Total Value with compound interest: $%.2f\n", result);
+    printf("\nSelect compounding frequency:\n");
+    printf("\n1. Daily\n2. Weekly\n3. Monthly\n4. Quarterly\n");
+    printf("5. Annually\n6. Continuous\n");
+    scanf("%d", &freq_choice);
+
+    switch(freq_choice) {
+        case 1: freq = DAILY; break;
+        case 2: freq = WEEKLY; break;
+        case 3: freq = MONTHLY; break;
+        case 4: freq = QUARTERLY; break;
+        case 5: freq = ANNUALLY; break;
+        case 6: freq = CONTINUOUS; break;
+        default: freq = MONTHLY;
+    }
+
+    result = calculate_compound_interest(principal, rate, time, freq);
+    printf("\nTotal Value with compound interest: %.2f\n", result);
 
     return 0;
 }
